fc5b145f19d8446791105b4daef2814207028593
myweb1 <119072006+myweb1@users.noreply.github.com>
2024-02-05 18:12:30 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -2,18 +2,30 @@ function setup() {
     canvas = createCanvas(300,300)
     sound.play()
     sound.setVolume (0.5)
-    sound.rate (0.5)
+    sound.rate (0.9)
     videoo = createCapture(VIDEO)
     ml500 = ml5.poseNet(videoo,listo)
     ml500.on("pose",net)
 }
-
+var yleft = 0
+var yright = 0
 function preload() {
     sound = loadSound("Revenge.mp3")
 }
 
 function draw() {
-
+    if (!sound.isPlaying() ) {
+sound.play()
+    }
+if (yright < 150 ) {
+    sound.rate(2)
+}
+else if (yright < 300) {
+    sound.rate(0.9)
+}
+else if (yright < 450) {
+    sound.rate(0.5)
+}
 }
 
 function net(k) {
