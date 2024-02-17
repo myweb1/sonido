49dc1e7d423d28f8d4bbb598015dbde3be32c39b
myweb1 <119072006+myweb1@users.noreply.github.com>
2024-02-16 19:07:37 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -9,6 +9,8 @@ function setup() {
 }
 var yleft = 0
 var yright = 0
+var confidenceright = 0
+var confidenceleft = 0
 function preload() {
     sound = loadSound("Revenge.mp3")
 }
@@ -17,22 +19,32 @@ function draw() {
     if (!sound.isPlaying() ) {
 sound.play()
     }
-if (yright < 150 ) {
-    sound.rate(2)
-}
-else if (yright < 300) {
-    sound.rate(0.9)
-}
-else if (yright < 450) {
-    sound.rate(0.5)
-}
+    if (confidenceright > 0.2 ) {
+
+        if (yright < 150 ) {
+            sound.rate(2)
+        }
+        else if (yright < 300) {
+            sound.rate(0.9)
+        }
+        else if (yright < 450) {
+            sound.rate(0.5)
+        }
+    }
+    if (confidenceleft > 0.2 ) {
+        volumen = 1 - (yleft / 450)
+        sound.setVolume(volumen)
+    }
 }
 
 function net(k) {
     if (k[0]) {
         yleft = k[0].pose.leftWrist.y
         yright = k[0].pose.rightWrist.y
+        confidenceleft = k[0].pose.leftWrist.confidence
+        confidenceright = k[0].pose.rightWrist.confidence
         console.log("yleft"+yleft)
+        
     }
 }
 
